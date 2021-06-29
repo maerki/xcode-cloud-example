ba61ca1377dd444a0863abe045e776abc35ac4b1
Nicolas Märki <maerki@ubique.ch>
2021-06-29 10:07:32 +0200

    Initial Commit

---
+++ XclodeCloudTest/XclodeCloudTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  XclodeCloudTestApp.swift
+//  XclodeCloudTest
+//
+//  Created by Nicolas Märki on 29.06.21.
+//
+
+import SwiftUI
+
+@main
+struct XclodeCloudTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
